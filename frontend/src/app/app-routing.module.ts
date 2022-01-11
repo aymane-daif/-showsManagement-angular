4433d74bec8e94035963007c1a3134b748e56b22
Aymane Daif <aymaned345@gmail.com>
2022-01-11 14:35:25 +0100

    add 404page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundHomeComponent } from './modules/pages/notFound/not-found-home/not-found-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/signup' },
+  { path: '**', component: NotFoundHomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AuthModule } from './modules/pages/auth/auth.module';
 import { SharedModule } from './modules/shared/shared.module';
+import { NotFoundHomeComponent } from './modules/pages/notFound/not-found-home/not-found-home.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, NotFoundHomeComponent],
   imports: [BrowserModule, AuthModule, SharedModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
