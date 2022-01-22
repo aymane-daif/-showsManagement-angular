86dd45eaa04fa61177a86207f722bf7d59dd4f6b
Aymane Daif <aymaned345@gmail.com>
2022-01-22 16:59:21 +0100

    :sparkles: add log out functionality

--- frontend/src/app/modules/pages/shows/components/shows-header/shows-header.component.ts
+++ frontend/src/app/modules/pages/shows/components/shows-header/shows-header.component.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { IsUserLoggedService } from 'src/app/modules/shared/services/is-user-logged.service';
 
 @Component({
   selector: 'app-shows-header',
   templateUrl: './shows-header.component.html',
-  styleUrls: ['./shows-header.component.css']
+  styleUrls: ['./shows-header.component.css'],
 })
 export class ShowsHeaderComponent implements OnInit {
-  username : String ='';
-  constructor() { }
+  username: String = '';
+  constructor(private userLogStatus: IsUserLoggedService) {}
 
   ngOnInit(): void {
     if (localStorage.getItem('username')) {
       this.username = localStorage.getItem('username') || '';
     }
   }
-
+  logOut() {
+    this.userLogStatus.logOut();
+  }
 }
